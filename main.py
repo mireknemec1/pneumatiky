d3390baefa0fd5b83fa7826c3f7712819641ef07
mireknemec1 <mirys30@seznam.cz>
2023-10-15 17:22:27 +0200

    4 butony nad sebou

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from kivy.graphics import Rectangle
 from kivy.utils import platform
 
 # Nastavení oranžové barvy pozadí pro celé okno
-Window.clearcolor = (1, 0.5, 0, 1)
+Window.clearcolor = (1, 0.6, 0, 1)
 
 class MainApp(App):
     def build(self):
         root = BoxLayout(orientation='vertical', spacing=0)   # Nastaveno spacing = okraje kolem butonů, obrázku dole
         
         # Horní lišta
-        top_bar = BoxLayout(size_hint_y=0.1, padding=5)  # Nastaveno padding = okraje kolem butonů na horní liště
+        top_bar = BoxLayout(size_hint_y=0.12, padding=5)  # Nastaveno padding = okraje kolem butonů na horní liště
         
         # Změna tlačítka na ikonu ozubeného kolečka
-        left_button = Button(background_normal='settings6.png', size_hint_x=0.12, background_color=(1, 1, 1, 1))
+        left_button = Button(background_normal='settings6.png', size_hint_x=0.13, background_color=(1, 1, 1, 1))
         
-        center_label = Label(text="PNEUMATIKY.CZ", size_hint_x=0.6, color=(1,1,1,1), font_size='18sp', bold=True) # Bílý text, tučně
+        center_label = Label(text="PrezutiZdarma.CZ", size_hint_x=0.6, color=(0.65, 0.1, 0, 1), font_size='20sp', bold=True)  # Bílý text, tučně
 
         right_button = Button(background_normal='info2.png', size_hint_x=0.14, background_color=(1, 1, 1, 1))
         
@@ -38,23 +38,19 @@ class MainApp(App):
         
         # Dolní část s čtyřmi tlačítky
         if platform == 'android':
-            # Rozložení pro Android: 2 tlačítka vedle sebe a další 2 pod nimi
             bottom_buttons = BoxLayout(size_hint_y=0.5, orientation='vertical', spacing=5, padding=0)  #spacing je mezera vodorovná mezi butony, 
-            
-            top_row = BoxLayout(size_hint_y=0.5, spacing=5)    # Přidáno spacing=5 mezera mezi tlačitky
-            button1 = Button(text="Servisy", background_color=(0.5, 0.5, 0.5, 1), bold=True)
-            button2 = Button(text="Pneumatiky", background_color=(0.5, 0.5, 0.5, 1), bold=True)
-            top_row.add_widget(button1)
-            top_row.add_widget(button2)
-            
-            bottom_row = BoxLayout(size_hint_y=0.5, spacing=5)  # Přidáno spacing=5 mezera mezi tlačitky
-            button3 = Button(text="Pomoc", background_color=(0.5, 0.5, 0.5, 1), bold=True)
-            button4 = Button(text="Rádce", background_color=(0.5, 0.5, 0.5, 1), bold=True)
-            bottom_row.add_widget(button3)
-            bottom_row.add_widget(button4)
-            
-            bottom_buttons.add_widget(top_row)
-            bottom_buttons.add_widget(bottom_row)
+
+            button1 = Button(text="PNEUMATIKY E-SHOP", background_color=(0.5, 0.5, 0.5, 1), bold=False, size_hint_y=0.25, font_size='18sp')
+            button2 = Button(text="Pneuservisy ZDARMA", background_color=(0.5, 0.5, 0.5, 1), bold=False, size_hint_y=0.25, font_size='18sp')
+            button3 = Button(text="Doporuč a ZÍSKÁVEJ", background_color=(0.5, 0.5, 0.5, 1), bold=False, size_hint_y=0.25, font_size='18sp')
+            button4 = Button(text="Rady Tipy Pomoc", background_color=(0.5, 0.5, 0.5, 1), bold=False, size_hint_y=0.25, font_size='18sp')
+
+
+            bottom_buttons.add_widget(button1)
+            bottom_buttons.add_widget(button2)
+            bottom_buttons.add_widget(button3)
+            bottom_buttons.add_widget(button4)
+
         else:
             # Rozložení pro ostatní platformy: všechna tlačítka vedle sebe
             bottom_buttons = BoxLayout(size_hint_y=0.2, spacing=10, padding=10)
